9e7cdaab38f65c61b9a4e35e2f23f7c924e8a601
Federico Aieta <maxe1111@hotmail.com>
2024-06-13 18:01:44 -0300

    Commit 2

---
+++ code/palabras.py
@@ -0,0 +1,34 @@
+diccionario = [
+    {"P, R, O, A, E, S": 
+    [
+        "aperos", "apreso", "aproes", "arpeos",
+        "aspero", "espora", "operas", "opresa",
+        "pareos", "pasero", "peoras", "posare", 
+        "raspeo", "repaso", "reposa", "separo",
+        "sopare", "sopear", "sopera", "pares",
+        "opera", "preso", "sopar", "rapos", "paseo",
+        "pera", "pose", "paro", "ropa", "sapo", 
+        "repo", "rapo", "pos", "rap", "res", "par", "por"
+    ],
+
+    "A, S, R, C, E, A": 
+    [
+        "aceras", "acreas", "arcase", "arceas",
+        "careas", "caresa", "casare", "casera",
+        "resaca", "sacare", "secara", "resaca",
+        "cesara", "escara", "seca", "caer", "rasca",
+        "res", "casar", "arcas", "asear", "secar",
+        "ser", "era", "cera", "crea", "esa", "sea",
+        "cara", "ras", "arca"
+    ],
+    "A, R, P, O, T, E": 
+    [
+        "aporte", "apreto", "atrope", "optare", "patero", 
+        "poetar", "portea", "potare", "potear", "potera", 
+        "potrea", "ropeta", "topare", "topear", "topera", 
+        "trapeo", "trapo", "par", "topar", "pare", "pato",
+        "tope", "rap", "rateo", "rapto", "tropa", "pote", 
+        "peor", "taper", "parte", "arte", "aro", "por", "rato"
+    ]
+    }
+    ]
