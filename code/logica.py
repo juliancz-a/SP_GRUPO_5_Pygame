a1f9d8c52191cb5573b1ac99026fe7c27d18a34c
Federico Aieta <maxe1111@hotmail.com>
2024-06-13 16:54:25 -0300

    Primer commit (FA)

--- code/logica.py
+++ code/logica.py
@@ -16,3 +16,48 @@ def shuffle (letras:list[str]) -> list[str]:
     return letras
     # test = reduce(lambda last, actual)
 print(shuffle(letras))
+
+
+diccionario = [
+    {"P, R, O, A, E, S": 
+    [
+        "aperos", "apreso", "aproes", "arpeos",
+        "aspero", "espora", "operas", "opresa",
+        "pareos", "pasero", "peoras", "posare", 
+        "raspeo", "repaso", "reposa", "separo",
+        "sopare", "sopear", "sopera", "pares",
+        "opera", "preso", "sopar", "rapos", "paseo",
+        "pera", "pose", "paro", "ropa", "sapo", 
+        "repo", "rapo", "pos", "rap", "res", "par"
+    ],
+
+    "R, C, A, E, S, E": 
+    [
+        "aceras", "acreas", "arcase", "arceas",
+        "careas", "caresa", "casare", "casera",
+        "resaca", "sacare", "sacare", "secara",
+        "cesara", "cesara", "escara"
+    ]
+    }
+    ]
+
+
+def coincidencias(combinacion: dict[str]):
+    set_dict = set(diccionario[0][combinacion])
+
+    palabra = input("Ingrese la palabra: ").strip().lower()
+
+    palabra_set = {palabra}
+
+    intersec = set_dict.intersection(palabra_set)
+
+    if len(intersec) > 0:
+        retorno = palabra
+    else:
+        retorno = False
+
+    return retorno
+
+palabra = coincidencias("R, C, A, E, S, E")
+print("Coincidencia:", palabra)
+
