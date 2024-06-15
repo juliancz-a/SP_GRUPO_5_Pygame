90cf909389aefb40a93b364fb3536d5c3d83453c
Federico Aieta <maxe1111@hotmail.com>
2024-06-15 18:08:17 -0300

    Commit Aieta 15/6

--- code/palabras.py
+++ code/palabras.py
@@ -3,12 +3,13 @@ diccionario = [
     [
         "aperos", "apreso", "aproes", "arpeos",
         "aspero", "espora", "operas", "opresa",
-        "pareos", "pasero", "peoras", "posare", 
+        "pareos", "pasero", "peoras", "posera", 
         "raspeo", "repaso", "reposa", "separo",
-        "sopare", "sopear", "sopera", "pares",
-        "opera", "preso", "sopar", "rapos", "paseo",
-        "pera", "pose", "paro", "ropa", "sapo", 
-        "repo", "rapo", "pos", "rap", "res", "par", "por"
+        "sopear", "sopera", "pares", "sopar", "rapos", "paseo",
+        "pera", "pose", "paro", "ropa", "sapo", "opera", "preso",
+        "repo", "rapo", "pos", "rap", "res", "par", "por",
+        "presa", "pesar", "persa", "peras", "paros", "aspeo",
+        "proa", "prea", "rape", "prosa"
     ],
 
     "A, S, R, C, E, A": 
@@ -19,18 +20,23 @@ diccionario = [
         "cesara", "escara", "seca", "caer", "rasca",
         "res", "casar", "arcas", "asear", "secar",
         "ser", "era", "cera", "crea", "esa", "sea",
-        "cara", "ras", "arca", "acera"
+        "cara", "ras", "arca", "acera", "arce", "caes"
+        "sacre", "cesar", "ceras", "caras", "caera", "carea",
+        "asca", "cas", "aca", "cea", "asa", "ara"
     ],
     "A, R, P, O, T, E": 
     [
         "aporte", "apreto", "atrope", "patero", "apero"
-        "poetar", "portea", "potare", "potear", "potera", 
+        "poetar", "portea", "potear", "potera", 
         "potrea", "ropeta", "topare", "topear", "topera", 
         "trapeo", "trapo", "par", "topar", "pare", "pato",
         "tope", "rap", "rateo", "rapto", "tropa", "pote", 
         "peor", "taper", "parte", "arte", "aro", "por", "rato"
         "opera", "peta", "opta", "poeta", "ropa", "ore", "pera",
-        "pero", "petar", "torpe", "parto", "optar", "arpeo"
+        "pero", "petar", "torpe", "parto", "optar", "arpeo",
+        "reo", "pare", "epa", "ero", "roe", "ate", "top", "apto",
+        "opte", "rape", "rapo", "tape", "tapo", "arpe", "ateo"
+        "otra", "arte", "arto", "potra", "trape", "peta", "topa"
     ],
     "C, A, E, R, T, O": 
     [
@@ -39,12 +45,16 @@ diccionario = [
         "corte", "corta", "actor", "toca", "taco", "rato",
         "roca", "crea", "cero", "arte", "acre", "caro", "ora",
         "era", "ceo", "oca", "ate", "caer", "careto", "arco"
-        "tera", "tero", "toc", "tac"
+        "tera", "tero", "toc", "tac", "acero", "aceto", "cetra",
+        "roce", "trace", "acto", "cera", "ocre", "terco", "terca"
+        "recta", "cotar", "acote", "cateo", "toce", "creo", "ateo"
+        "rota", "peta", "petar", "topa", "top", "tapo", "tocar"
+        "eco", "tace", "cera", "roe", 
     ],
     "R, L, T, E, O, M": 
     [
         "merlot", "metro", "merlo", "moler", "tole", "mole",
-        "lote", "mote", "rote", "tremo", "telo", "tremol",
+        "lote", "mote", "rote", "tremo", "telo", "tero"
         "merol", "temor", "remo", "oler", "ore", "reo", "rol",
         "leo", "trol", "lero", "reto", "mol", "meto", "termo",
         "motel", "ero", "roel", "more", "tome"
