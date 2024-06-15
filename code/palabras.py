bf9fecd6eb9113f994845690601139fb4a5978fc
Federico Aieta <maxe1111@hotmail.com>
2024-06-15 16:37:52 -0300

    Commit AIETA 15/6

--- code/palabras.py
+++ code/palabras.py
@@ -1,5 +1,5 @@
 diccionario = [
-    {"P, R, O, A, E, S": 
+    {"P, R, O, A, E, S":
     [
         "aperos", "apreso", "aproes", "arpeos",
         "aspero", "espora", "operas", "opresa",
@@ -19,24 +19,41 @@ diccionario = [
         "cesara", "escara", "seca", "caer", "rasca",
         "res", "casar", "arcas", "asear", "secar",
         "ser", "era", "cera", "crea", "esa", "sea",
-        "cara", "ras", "arca"
+        "cara", "ras", "arca", "acera"
     ],
     "A, R, P, O, T, E": 
     [
-        "aporte", "apreto", "atrope", "optare", "patero", 
+        "aporte", "apreto", "atrope", "patero", "apero"
         "poetar", "portea", "potare", "potear", "potera", 
         "potrea", "ropeta", "topare", "topear", "topera", 
         "trapeo", "trapo", "par", "topar", "pare", "pato",
         "tope", "rap", "rateo", "rapto", "tropa", "pote", 
         "peor", "taper", "parte", "arte", "aro", "por", "rato"
-        "opera", "peta", "opta", "poeta", "ropa", "ore"
+        "opera", "peta", "opta", "poeta", "ropa", "ore", "pera",
+        "pero", "petar", "torpe", "parto", "optar", "arpeo"
     ],
-    "C, A, E, R, T, O": [
-        "tocare", "tacero", "rector", "cortar","torea",
+    "C, A, E, R, T, O": 
+    [
+        "tocare", "tacero", "rector", "cortar", "torea",
         "torca", "tarco", "recta", "recto", "cetro",
         "corte", "corta", "actor", "toca", "taco", "rato",
         "roca", "crea", "cero", "arte", "acre", "caro", "ora",
-        "era", "ceo", "oca", "ate", "caer", "careto"
-        ]
+        "era", "ceo", "oca", "ate", "caer", "careto", "arco"
+        "tera", "tero", "toc", "tac"
+    ],
+    "R, L, T, E, O, M": 
+    [
+        "merlot", "metro", "merlo", "moler", "tole", "mole",
+        "lote", "mote", "rote", "tremo", "telo", "tremol",
+        "merol", "temor", "remo", "oler", "ore", "reo", "rol",
+        "leo", "trol", "lero", "reto", "mol", "meto", "termo",
+        "motel", "ero", "roel", "more", "tome"
+    ],
+    "L, C, I, O, N, A": 
+    [
+        "colina", "caolin", "clona", "clon", "loca", "cal",
+        "clan", "cian", "inca", "laico", "licaon", "col",
+        "lanio", "lona", "ocal", "canil"
+    ]
     }
     ]
