9de6aef44a122c321c14c41445a59b83eddbe48d
juliancz-a <juliancaceres204@gmail.com>
2024-06-15 22:52:10 -0300

    Box/Input_boxes

---
+++ code/class_box.py
@@ -0,0 +1,13 @@
+class Box:
+    #CONSTRUCTOR
+    def __init__(self) -> None:
+        self.color_principal = None
+        self.color_secundario = None
+        self.color_click = None
+        self.dimensiones = (None,None)
+        self.posicion = (None,None)
+        self.texto = None
+
+    
+    def presionar (self, mouse_pos:tuple):
+        pass
