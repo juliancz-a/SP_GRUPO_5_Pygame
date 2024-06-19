47a124fa1538fb6153b83acfa75508b9ad16794a
Federico Aieta <maxe1111@hotmail.com>
2024-06-19 20:03:14 -0300

    ddas

--- code/constantes.py
+++ code/constantes.py
@@ -10,7 +10,7 @@ BORDE_1 = "gray0"
 PRESS_SOUND = r"code\data\sound\press_button.wav"
 MENU_MUSIC = r"code\data\sound\musica_menu.mp3"
 PLAY_MUSIC = r"code\data\sound\play_music.wav"
-
+CARTAS = r"code\data\img\card_example.png"
 
 MENU_BACKGROUND = r"code\data\img\Menu_background.png"
 PLAY_BACKGROUND_1 = r"code\data\img\Play_background_1.png"
