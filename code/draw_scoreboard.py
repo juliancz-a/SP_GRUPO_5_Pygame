9e2ca2ef2af62475c7654d17ac9e4b4ea155dcc3
juliancz-a <juliancaceres204@gmail.com>
2024-06-25 23:17:49 -0300

    scre

--- code/draw_scoreboard.py
+++ code/draw_scoreboard.py
@@ -9,29 +9,40 @@ class Scoreboard_2:
 
         self.font = pygame.font.Font(font)
     def draw (self):
-        x = self.draw_pos[0]
         y = self.draw_pos[1]
         
-        Scoreboard_2.draw_header(self.surface, ["NOMBRE", "PUNTAJE", "PARTIDAS"], self.font, (800, 20))
+        header_pos = Scoreboard_2.draw_header(self.surface, ["nombre", "puntos", "partidas"], self.font, (800, 20))
         
         for player in self.player_list:
-            player_name = self.font.render(player["nombre"], True, "black")
-            player_points = self.font.render(str(player["puntos"]), True, "black")
-            player_matches = self.font.render(str(player["partidas"]), True, "black")
-            self.surface.blit(player_name, (x,y))
-            x += 100
-            self.surface.blit(player_points, (x,y))
-            x += 100
-            self.surface.blit(player_matches, (x,y))
-            x = 800
             y += 10
+            for header in header_pos:
+                for key,value in header.items():
+                    print(player[key])
+                    header_data = self.font.render(str(player[key]), True, "black")
+                    self.surface.blit(header_data, (value, y))
+
+            # player_name = self.font.render(player["nombre"], True, "black")
+            # player_points = self.font.render(str(player["puntos"]), True, "black")
+            # player_matches = self.font.render(str(player["partidas"]), True, "black")
+            # self.surface.blit(player_name, (x,y))
+            # x += 100
+            # self.surface.blit(player_points, (x,y))
+            # x += 100
+            # self.surface.blit(player_matches, (x,y))
+            # x = 800
+            # y += 10
     
-    def draw_header (surface:pygame.Surface, headers:list[str], font, initial_pos:tuple):
+    def draw_header (surface:pygame.Surface, header_elements:list[str], font, initial_pos:tuple) -> list:
         x = initial_pos[0]
         y = initial_pos[1]
+        header_pos = []
 
-        for header in headers:
+        for header in header_elements:
             header_text = font.render(header, True, "black")
             surface.blit(header_text, (x,y))
 
+            header_pos.append({header: x})
+
             x += len(header) + 100
+       
+        return header_pos
